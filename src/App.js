4cba05789ab605e1ae28ff1a89c7e45a871eb741
Hakan Bilgi <hknblg34@gmail.com>
2022-10-13 19:21:43 +0300

    ...

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-import "./App.css";
+import { useState } from "react";
 import State from "./components/State";
 
 function App() {
-  return <State />;
+  const [name, setName] = useState("Mehmet");
+  const [age, setAge] = useState(29);
+  const [friends, setFriends] = useState(["Ali", "Veli"]);
+
+  return (
+    <>
+    <State/>
+      <h1>Merhaba {name}</h1>
+      <button onClick={() => setName("Ali")}>Click</button>
+      <hr />
+      <h1>yaş:{age}</h1>
+      <button onClick={() => setAge(88)}>Click</button>
+      <hr />
+      {friends.map((item) => (
+        <div>{item}</div>
+      ))}
+      <button onClick={() => setFriends(["Noah"])}>add new friend</button>
+    </>
+  );
 }
 
 export default App;
